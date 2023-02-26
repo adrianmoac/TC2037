e7df3d151079751c24fd136cd6adc2b895cf9c2b
Adrián Moras Acuña <adrianma@Adrians-MacBook-Pro.local>
2023-02-25 21:25:48 -0600

    No errors

--- lib/bowling.ex
+++ lib/bowling.ex
@@ -1,18 +1,13 @@
 defmodule Bowling do
-  @moduledoc """
-  Documentation for `Bowling`.
-  """
-
-  @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> Bowling.hello()
-      :world
-
-  """
-  def hello do
-    :world
+  def score([]), do: 0
+    def score([[10,nil|_]|[[10,_|_]|_] = others]) do
+    if hd(tl(hd(others))) != 10 do
+      30 + score(others)
+    else
+      30 + hd(tl(hd(others))) + score(others)
+    end
   end
+  def score([[10,nil|_c]|[[c,d|_]|_] = others]), do: 10 + c + d + score(others)
+  def score([[a,b|_c]|others]) when a + b == 10, do: 10 + hd(hd(others)) + score(others)
+  def score([[a,b|_c]|others]), do: a + b + score(others)
 end
